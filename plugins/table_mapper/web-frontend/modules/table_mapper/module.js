2bf56852e783d2d465a3d034265b180c078e7d28
李思海 <lsh@haocai.com>
2026-03-09 10:05:15 +0800

    修改配置

--- plugins/table_mapper/web-frontend/modules/table_mapper/module.js
+++ plugins/table_mapper/web-frontend/modules/table_mapper/module.js
@@ -6,8 +6,5 @@ import path from 'path'
 
 export default function TableMapperModule() {
   // 添加插件
-  this.addPlugin({
-    name: 'tableMapper',
-    plugin: path.resolve(__dirname, 'plugin.js'),
-  })
+  this.addPlugin(path.resolve(__dirname, 'plugin.js'))
 }
